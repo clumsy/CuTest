9268bb17d586b569ce9085889a073776209667e6
Alexander Jipa <alexander.jipa@gmail.com>
2017-12-22 00:12:22 -0500

    initial commit

---
+++ test/AllTests.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define CUTEST_IMPLEMENTATION
+#include "../CuTest.h"
+
+CuSuite* CuGetSuite();
+CuSuite* CuStringGetSuite();
+
+void RunAllTests(void) {
+    CuString *output = CuStringNew();
+    CuSuite *suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, CuGetSuite());
+    CuSuiteAddSuite(suite, CuStringGetSuite());
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void) {
+    RunAllTests();
+}
